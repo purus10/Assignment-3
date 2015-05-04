81e8da1b5bb8bb90573bae1b5872bb4444d6889c
Wilfred Young <goforit88@gmail.com>
2015-05-04 08:30:24 -0400

    Added Music, and background but only as a test

---
+++ RPG Project/Assets/Script/Music.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour 
+{
+
+    public AudioSource Musical;
+    public AudioSource Bettle;
+    public AudioSource Ambi2;
+
+
+	// Use this for initialization
+	void Awake () 
+    {
+        Musical.Play();
+        Musical.volume = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Musical.Play();
+        Musical.volume = 0.5f;
+	}
+}
